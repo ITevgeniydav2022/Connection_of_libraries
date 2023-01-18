dfbce282f77753ba41dcef48210ce5222da5223d
ITevgeniydav2022 <defox2022@gmail.com>
2023-01-18 20:01:45 +0300

    Первый коммит

---
+++ src/main/java/me/davydovep/recipesapp_4/RecipesApp4Application.java
@@ -0,0 +1,13 @@
+package me.davydovep.recipesapp_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesApp4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipesApp4Application.class, args);
+    }
+
+}
---
+++ src/main/java/me/davydovep/recipesapp_4/controller/IngredientController.java
@@ -0,0 +1,46 @@
+package me.davydovep.recipesapp_4.controller;
+
+
+import me.davydovep.recipesapp_4.model.Ingredient;
+import me.davydovep.recipesapp_4.service.IngredientService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/ingredient")
+public class IngredientController {
+
+    private final IngredientService ingredientService;
+
+    public IngredientController(IngredientService ingredientService) {
+        this.ingredientService = ingredientService;
+    }
+
+    @PostMapping
+    public Ingredient add(@RequestBody Ingredient ingredient) {
+        return ingredientService.add(ingredient);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Ingredient> get(@PathVariable long id) {
+        return ResponseEntity.of(ingredientService.get(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Ingredient> update(@PathVariable long id,
+                                             @RequestBody Ingredient ingredient) {
+        return ResponseEntity.of(ingredientService.update(id, ingredient));
+    }
+    @DeleteMapping ("/{id}")
+    public ResponseEntity<Ingredient> delete(@PathVariable long id) {
+        return ResponseEntity.of(ingredientService.delete(id));
+    }
+
+    @GetMapping
+    public Map<Long, Ingredient> getAll() {
+        return ingredientService.getAll();
+    }
+
+}
